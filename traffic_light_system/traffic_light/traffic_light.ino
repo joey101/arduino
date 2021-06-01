729afaa67eb399f5d3899617b86f857e9b30d967
joey101 <jawad.tanana@gmail.com>
2021-06-01 20:57:48 +1000

    traffic light system

--- traffic_light_system/traffic_light/traffic_light.ino
+++ traffic_light_system/traffic_light/traffic_light.ino
@@ -1,15 +1,23 @@
+int red = 13;
+int yellow = 12;
+int green = 11;
+
+int red_ped = 10;
+int green_ped = 9;
+
+int button = 8;
 int buttonState = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  red = pinmode(13,OUTPUT); // RED
-  yellow = pinmode(12,OUTPUT); // YELLOW
-  green = pinmode(11,OUTPUT); // GREEN
+  pinMode(red,OUTPUT); // RED
+  pinMode(yellow,OUTPUT); // YELLOW
+  pinMode(green,OUTPUT); // GREEN
 
-  red_ped = pinmode(10,OUTPUT); // RED pedestrian
-  green_ped = pinmode(9,OUTPUT); // GREEN pedestrian
+  pinMode(red_ped,OUTPUT); // RED pedestrian
+  pinMode(green_ped,OUTPUT); // GREEN pedestrian
 
-  button = pinmode(8,INPUT); // Button
+  pinMode(button,INPUT); // Button
   
 
 }
@@ -17,11 +25,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.begin(9600);
-  digitalWrite(red, HIGH)
-  //buttonState = digitalREAD(button);
+  
+  buttonState = digitalRead(button);
+  
+  if (buttonState == HIGH) {
+    
+    digitalWrite(red, HIGH);
+    //delay(500);
+    digitalWrite(yellow, HIGH);
+    //delay(500);
+    digitalWrite(green, HIGH);
 
-  // if (buttonSTATE == HIGH) {
-  //    delay(3000);
+    digitalWrite(red_ped, HIGH);
+    digitalWrite(green_ped, HIGH);
          
-  //}
+  }
+  else if (buttonState == LOW) {
+    
+    digitalWrite(red, LOW);
+    
+    digitalWrite(yellow, LOW);
+    
+    digitalWrite(green, LOW);
+     digitalWrite(red_ped, LOW);
+    digitalWrite(green_ped, LOW);
+  }
 }
